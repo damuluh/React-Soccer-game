4597dbba6aacfd2e469e51d642d4a41d039925f3
muluh <muluh@mymuluh.com>
2020-08-04 15:25:01 -0400

    restrusture a basic react-soccer-game into sperate components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Components/ScoreBoard/ScoreBoard.css'
+import Game from './Components/Games/Game'
+import Valen from './Components/Assets/Images/200px-Valenciacf.svg.png'
+import Bar from './Components/Assets/Images/bas.jpeg'
+import RealM from './Components/Assets/Images/real.jpg'
+import AtleticoImage from './Components/Assets/Images/Atletico.png'
 
-function App() {
+
+
+
+
+
+function App(props) {
+  const Real = {
+    name: 'Real Madrid',
+    logoSrc: RealM,
+  };
+  const Barcelona = {
+    name: 'FC Barcelona',
+    logoSrc: Bar,
+  };
+  const Atletico = {
+    name: 'Atletico Madrid',
+    logoSrc: AtleticoImage,
+  };
+  const Valencia = {
+    name: 'Valencia CF',
+    logoSrc: Valen,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="UEFA Champions League Playing At Santiago Bernabéu Stadium"
+        homeTeam={Real}
+        visitingTeam={Barcelona}
+      />
+      <Game
+        venue="La Liga Soccer Playing At Mestalla Stadium"
+        homeTeam={Atletico}
+        visitingTeam={Valencia}
+     />
+    
+
+     
     </div>
   );
 }
-
-export default App;
+export default App
